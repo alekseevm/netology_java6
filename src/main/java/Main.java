466fa36e8114f9c3784de79bb87dff1f2f531f65
alekseev <alekseev.m@me.com>
2022-07-19 16:15:25 +0300

    fixed #1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,11 +4,18 @@ public class Main {
 
     long amount = 1000_60;
     boolean registered = true;
-
-   service.calculate(amount, registered);
-
+    long expected = 30;
 
 
+   long actual  = service.calculate(amount, registered);
+    assertEquals(expected, actual);
+    
+
+
+
+  }
+
+  private static void assertEquals(long expected, long actual) {
   }
 }
 
